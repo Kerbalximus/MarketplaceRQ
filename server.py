d6bac516681f0a3062a10b56fd2eadc6df440eb9
Kerbalximus <69394642+Kerbalximus@users.noreply.github.com>
2021-06-05 18:34:36 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 sock = socket.socket()
 sock.bind(('', 9090))
@@ -8,9 +9,19 @@ conn, addr = sock.accept()
 print ('connected:', addr)
 
 while True:
-    data = conn.recv(1024)
+    data = conn.recv(102400)
     if not data:
         break
-    conn.send(data.upper())
+
+    
+    conn.send(b"DATA find in hosting RQ")
 
 conn.close()
+
+data = data.decode()
+data_json = json.load(data)
+
+with open(f"./python-data/{data_json['name']}", "w") as python_file:
+    python_file.write(data_json['data'])
+
+print(f"OK, create mode {data_json['name']}")
